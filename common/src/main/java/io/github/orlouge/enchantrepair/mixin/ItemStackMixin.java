dcc59cea6a9db64d62bf57d78607b9e9845c8bcc
Koby Lewis <kobyjlewis@gmail.com>
2025-08-19 10:22:17 -0600

    Update enchanting cursed items

--- common/src/main/java/io/github/orlouge/enchantrepair/mixin/ItemStackMixin.java
+++ common/src/main/java/io/github/orlouge/enchantrepair/mixin/ItemStackMixin.java
@@ -1,16 +1,20 @@
 package io.github.orlouge.enchantrepair.mixin;
 
+import net.minecraft.component.type.ItemEnchantmentsComponent;
 import net.minecraft.enchantment.EnchantmentHelper;
 import net.minecraft.enchantment.Enchantments;
 import net.minecraft.item.ItemStack;
+import net.minecraft.registry.tag.EnchantmentTags;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
 
 @Mixin(ItemStack.class)
 public class ItemStackMixin {
-//    @Redirect(method = "isEnchantable", at = @At(value = "INVOKE", target = "Lnet/minecraft/item/ItemStack;hasEnchantments()Z"))
-//    public boolean cursedItemsAreEnchantable(ItemStack stack) {
-//        return EnchantmentHelper.get(stack).keySet().stream().anyMatch(ench -> !ench.isCursed() || ench == Enchantments.VANISHING_CURSE);
-//    }
+    @Redirect(method = "isEnchantable", at = @At(value = "INVOKE", target = "Lnet/minecraft/component/type/ItemEnchantmentsComponent;isEmpty()Z"))
+    public boolean cursedItemsAreEnchantable(ItemEnchantmentsComponent iec) {
+        return !iec.getEnchantments().stream().anyMatch(ench ->
+                !ench.isIn(EnchantmentTags.CURSE) || ench.getKey().get().equals(Enchantments.VANISHING_CURSE)
+        );
+    }
 }
