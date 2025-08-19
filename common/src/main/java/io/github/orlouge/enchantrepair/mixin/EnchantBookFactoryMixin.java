d67993811d3dedbf5989b4d5fe069ec13466f483
Koby Lewis <kobyjlewis@gmail.com>
2025-08-19 14:41:47 -0600

    Update villagers selling vanishing books

--- common/src/main/java/io/github/orlouge/enchantrepair/mixin/EnchantBookFactoryMixin.java
+++ common/src/main/java/io/github/orlouge/enchantrepair/mixin/EnchantBookFactoryMixin.java
@@ -1,12 +1,13 @@
 package io.github.orlouge.enchantrepair.mixin;
 
 import io.github.orlouge.enchantrepair.Config;
+import net.minecraft.component.DataComponentTypes;
+import net.minecraft.component.type.ItemEnchantmentsComponent;
 import net.minecraft.enchantment.EnchantmentHelper;
-import net.minecraft.enchantment.EnchantmentLevelEntry;
 import net.minecraft.enchantment.Enchantments;
 import net.minecraft.entity.Entity;
-//import net.minecraft.item.EnchantedBookItem;
 import net.minecraft.item.ItemStack;
+import net.minecraft.registry.RegistryKeys;
 import net.minecraft.util.math.random.Random;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -14,11 +15,16 @@ import org.spongepowered.asm.mixin.injection.ModifyVariable;
 
 @Mixin(targets = "net/minecraft/village/TradeOffers$EnchantBookFactory")
 public class EnchantBookFactoryMixin {
-//    @ModifyVariable(method = "create", at = @At(value = "STORE", ordinal = 0))
-//    public ItemStack addCurseOfVanishing(ItemStack stack, Entity entity, Random random) {
-//        if (Config.CURSE_TRADED_BOOKS) {
-//            EnchantedBookItem.addEnchantment(stack, new EnchantmentLevelEntry(Enchantments.VANISHING_CURSE, 1));
-//        }
-//        return stack;
-//    }
+    @ModifyVariable(method = "create", at = @At(value = "STORE", ordinal = 0))
+    public ItemStack addCurseOfVanishing(ItemStack stack, Entity entity, Random random) {
+        if (Config.CURSE_TRADED_BOOKS) {
+            ItemEnchantmentsComponent.Builder builder = new ItemEnchantmentsComponent.Builder(
+                    stack.getComponents().get(DataComponentTypes.STORED_ENCHANTMENTS)
+            );
+            var vanishing = entity.getRegistryManager().getOrThrow(RegistryKeys.ENCHANTMENT).getOrThrow(Enchantments.VANISHING_CURSE);
+            builder.add(vanishing, 1);
+            EnchantmentHelper.set(stack, builder.build());
+        }
+        return stack;
+    }
 }
