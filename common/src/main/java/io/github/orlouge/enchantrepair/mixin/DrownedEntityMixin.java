58b37c1f72aa6fc0db2742b0110a057ccffa92bb
Koby Lewis <kobyjlewis@gmail.com>
2025-08-19 14:52:47 -0600

    Update drowned dropping loyalty

--- common/src/main/java/io/github/orlouge/enchantrepair/mixin/DrownedEntityMixin.java
+++ common/src/main/java/io/github/orlouge/enchantrepair/mixin/DrownedEntityMixin.java
@@ -1,15 +1,20 @@
 package io.github.orlouge.enchantrepair.mixin;
 
 import io.github.orlouge.enchantrepair.Config;
+import net.minecraft.component.type.ItemEnchantmentsComponent;
+import net.minecraft.enchantment.Enchantment;
 import net.minecraft.enchantment.EnchantmentHelper;
 import net.minecraft.enchantment.Enchantments;
 import net.minecraft.entity.EntityType;
+import net.minecraft.entity.EquipmentDropChances;
 import net.minecraft.entity.EquipmentSlot;
 import net.minecraft.entity.SpawnReason;
 import net.minecraft.entity.mob.DrownedEntity;
 import net.minecraft.entity.mob.ZombieEntity;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
+import net.minecraft.registry.RegistryKeys;
+import net.minecraft.registry.entry.RegistryEntry;
 import net.minecraft.util.math.random.Random;
 import net.minecraft.world.LocalDifficulty;
 import net.minecraft.world.World;
@@ -30,13 +35,17 @@ public class DrownedEntityMixin extends ZombieEntity {
         super(world);
     }
 
-//    @Inject(method = "initEquipment", at = @At(value = "INVOKE", ordinal = 0, target = "Lnet/minecraft/entity/mob/DrownedEntity;equipStack(Lnet/minecraft/entity/EquipmentSlot;Lnet/minecraft/item/ItemStack;)V"))
-//    public void equipLoyalty(Random random, LocalDifficulty localDifficulty, CallbackInfo ci) {
-//        if (Config.DROWNED_SPAWNS_WITH_LOYALTY && random.nextInt(5) == 0) {
-//            ItemStack book = new ItemStack(Items.ENCHANTED_BOOK);
-//            EnchantmentHelper.set(Map.of(Enchantments.LOYALTY, random.nextInt(3) + 1), book);
-//            this.equipStack(EquipmentSlot.OFFHAND, book);
-//            this.updateDropChances(EquipmentSlot.OFFHAND);
-//        }
-//    }
+    @Inject(method = "initEquipment", cancellable=true, at = @At(value = "INVOKE", ordinal = 0, target = "Lnet/minecraft/entity/mob/DrownedEntity;equipStack(Lnet/minecraft/entity/EquipmentSlot;Lnet/minecraft/item/ItemStack;)V"))
+    public void equipLoyalty(Random random, LocalDifficulty localDifficulty, CallbackInfo ci) {
+        if (Config.DROWNED_SPAWNS_WITH_LOYALTY && random.nextInt(5) == 0) {
+            ItemStack book = new ItemStack(Items.ENCHANTED_BOOK);
+            ItemEnchantmentsComponent.Builder builder = new ItemEnchantmentsComponent.Builder(ItemEnchantmentsComponent.DEFAULT);
+            RegistryEntry<Enchantment> loyalty = getRegistryManager().getOrThrow(RegistryKeys.ENCHANTMENT).getOrThrow(Enchantments.LOYALTY);
+            builder.add(loyalty, random.nextInt(3) + 1);
+            EnchantmentHelper.set(book, builder.build());
+            this.equipStack(EquipmentSlot.OFFHAND, book);
+            this.setEquipmentDropChance(EquipmentSlot.OFFHAND, EquipmentDropChances.DEFAULT_CHANCE);
+            ci.cancel();
+        }
+    }
 }
