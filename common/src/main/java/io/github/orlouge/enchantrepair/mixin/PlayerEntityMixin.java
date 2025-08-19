0522ceb1df90b91bb4732eb0f53baec6c9409dc5
Koby Lewis <kobyjlewis@gmail.com>
2025-08-19 09:56:31 -0600

    Update experience dropping

--- common/src/main/java/io/github/orlouge/enchantrepair/mixin/PlayerEntityMixin.java
+++ common/src/main/java/io/github/orlouge/enchantrepair/mixin/PlayerEntityMixin.java
@@ -5,6 +5,7 @@ import net.minecraft.entity.EntityType;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemStack;
+import net.minecraft.server.world.ServerWorld;
 import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
@@ -18,9 +19,9 @@ public abstract class PlayerEntityMixin extends LivingEntity {
 //    @Shadow
 //    protected int enchantmentTableSeed;
 //
-//    @Shadow public float experienceProgress;
-//
-//    @Shadow public int experienceLevel;
+    @Shadow public float experienceProgress;
+
+    @Shadow public int experienceLevel;
 //
     protected PlayerEntityMixin(EntityType<? extends LivingEntity> entityType, World world) {
         super(entityType, world);
@@ -34,22 +35,22 @@ public abstract class PlayerEntityMixin extends LivingEntity {
 //        }
 //    }
 //
-//    @Inject(method = "getXpToDrop", at = @At("RETURN"), cancellable = true)
-//    public void setXpToDrop(CallbackInfoReturnable<Integer> cir) {
-//        if (Config.XP_LEVELS_LOST_ON_DEATH >= 0) {
-//            double xp = 0;
-//            if (Config.XP_LOST_DROPPED > 0 && Config.XP_LEVELS_LOST_ON_DEATH > 0 && cir.getReturnValue() > 0) {
-//                xp = xpAtLevel(this.experienceLevel) + xpDiffAtLevel(this.experienceLevel) * this.experienceProgress;
-//                int newLevel = Math.max(0, this.experienceLevel - Config.XP_LEVELS_LOST_ON_DEATH);
-//                double newProgress = this.experienceLevel - Config.XP_LEVELS_LOST_ON_DEATH >= 0 ? this.experienceProgress : 0;
-//
-//                xp -= xpAtLevel(newLevel) + xpDiffAtLevel(newLevel) * newProgress;
-//                xp *= Config.XP_LOST_DROPPED * 0.01;
-//            }
-//            cir.setReturnValue((int) Math.floor(xp));
-//            cir.cancel();
-//        }
-//    }
+    @Inject(method = "getExperienceToDrop", at = @At("RETURN"), cancellable = true)
+    public void setXpToDrop(ServerWorld world, CallbackInfoReturnable<Integer> cir) {
+        if (Config.XP_LEVELS_LOST_ON_DEATH >= 0) {
+            double xp = 0;
+            if (Config.XP_LOST_DROPPED > 0 && Config.XP_LEVELS_LOST_ON_DEATH > 0 && cir.getReturnValue() > 0) {
+                xp = xpAtLevel(this.experienceLevel) + xpDiffAtLevel(this.experienceLevel) * this.experienceProgress;
+                int newLevel = Math.max(0, this.experienceLevel - Config.XP_LEVELS_LOST_ON_DEATH);
+                double newProgress = this.experienceLevel - Config.XP_LEVELS_LOST_ON_DEATH >= 0 ? this.experienceProgress : 0;
+
+                xp -= xpAtLevel(newLevel) + xpDiffAtLevel(newLevel) * newProgress;
+                xp *= Config.XP_LOST_DROPPED * 0.01;
+            }
+            cir.setReturnValue((int) Math.floor(xp));
+            cir.cancel();
+        }
+    }
 
     private static double xpAtLevel(int level) {
         if (level <= 16) return level * level + 6 * level;
