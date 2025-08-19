ca783c5beea9448c021bab3893cd69916c35ca27
Koby Lewis <kobyjlewis@gmail.com>
2025-08-19 12:04:10 -0600

    Update preventing xp from mending gear

--- common/src/main/java/io/github/orlouge/enchantrepair/mixin/ExperienceOrbEntityMixin.java
+++ common/src/main/java/io/github/orlouge/enchantrepair/mixin/ExperienceOrbEntityMixin.java
@@ -3,6 +3,7 @@ package io.github.orlouge.enchantrepair.mixin;
 import io.github.orlouge.enchantrepair.Config;
 import net.minecraft.entity.ExperienceOrbEntity;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.server.network.ServerPlayerEntity;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -10,11 +11,11 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(ExperienceOrbEntity.class)
 public class ExperienceOrbEntityMixin {
-//	@Inject(at = @At("HEAD"), method = "repairPlayerGears", cancellable = true)
-//	private void onRepairPlayerGears(PlayerEntity player, int amount, CallbackInfoReturnable<Integer> info) {
-//		if (Config.DISABLE_MENDING_XP_REPAIR) {
-//			info.setReturnValue(amount);
-//			info.cancel();
-//		}
-//	}
+	@Inject(at = @At("HEAD"), method = "repairPlayerGears", cancellable = true)
+	private void onRepairPlayerGears(ServerPlayerEntity player, int amount, CallbackInfoReturnable<Integer> info) {
+		if (Config.DISABLE_MENDING_XP_REPAIR) {
+			info.setReturnValue(amount);
+			info.cancel();
+		}
+	}
 }
